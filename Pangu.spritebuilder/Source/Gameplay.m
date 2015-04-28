0916adff1aec7c71a186743c6d433f385c1c6419
Aprilcj <april.jianchen@gmail.com>
2015-04-28 16:24:20 -0400

    change particle effect

--- Pangu.spritebuilder/Source/Gameplay.m
+++ Pangu.spritebuilder/Source/Gameplay.m
@@ -308,13 +308,11 @@ static Gameplay* s_currentGame;
 - (void)share{
     FBSDKShareLinkContent *content = [[FBSDKShareLinkContent alloc] init];
     
-    // this should link to FB page for your app or AppStore link if published
     content.contentURL = [NSURL URLWithString:@"https://www.facebook.com/makeschool"];
-    // URL of image to be displayed alongside post
     content.imageURL = [NSURL URLWithString:@"https://git.makeschool.com/MakeSchool-Tutorials/News/f744d331484d043a373ee2a33d63626c352255d4//663032db-cf16-441b-9103-c518947c70e1/cover_photo.jpeg"];
     // title of post
-    content.contentTitle = [NSString stringWithFormat:@"test!"];
-    content.contentDescription = @"Details of test";
+    content.contentTitle = [NSString stringWithFormat:@"I've got %d in OceanCraft!", _scoreValue];
+    content.contentDescription = @"A cool marine shooting game.";
     
     [FBSDKShareDialog showFromViewController:[CCDirector sharedDirector]
                                  withContent:content
